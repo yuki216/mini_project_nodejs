09ff258c5d55be3a58bc22be2b7b86d6e854a609
Yuki F <fachry.yuki@gmail.com>
2022-01-19 21:52:08 +0700

    add fiture handler merchant

--- app/handler/MerchantHandler.js
+++ app/handler/MerchantHandler.js
@@ -7,12 +7,44 @@ const sequelize = require(`../infratructures/database`);
 
 module.exports = {
     register: async (req, res) => {
-        const data = await merchant.findAll();
-          console.log(data)
-        res.status(200).send(`Registration has been saved`);
+        const data = await merchant.create({
+            phone_number: req.body.phone_number,
+            password: req.body.password,
+            name:req.body.name,
+            address:req.body.address,
+            join_date:req.body.join_date
+        }).then(function(){
+            res.json({
+                "code" : "success",
+                "Message" : "Created merchant success.",
+                "merchant" : data
+            });
+        }).catch(function (err) {
+            res.status(500).json({
+                "code" : "error",
+                "Message" : err.parent.sqlMessage
+            })
+        });
     },
 
     remove: async (req, res) => {
-        res.status(200).send(`Registration has been saved`);
+        const dataMerchant = {
+            where: {
+              id: parseInt(req.params.id),
+            },
+          };
+      
+          const deleteMerchant = await merchant.destroy(dataMerchant).then(function(item){
+                res.status((item === 0)?500:200).json({
+                    "code" : (item === 0)?"error":"success",
+                    "Message" : (item === 0)?"ID Not Found.":"Delete merchant success."
+                });
+            }).catch(function (err) {
+                res.status(500).json({
+                    "code" : "error",
+                    "Message" : err
+                })
+            });
+       
     },
 }
--- app/routes/merchant.js
+++ app/routes/merchant.js
@@ -4,8 +4,6 @@ const merchantHandler = require(`../handler/MerchantHandler`)
 
 router.post("/register", merchantHandler.register)
 
-router.post("/remove", function(req,res){
-    res.status(200).send("success")
-})
+router.post("/remove/:id", merchantHandler.remove)
 
 module.exports = router;
